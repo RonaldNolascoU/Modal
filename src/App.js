0209c55702eecb6391a37ab4f90b8d143848dad4
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2020-10-21 20:53:00 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ModalExample from './Modal'
+import { Button, Modal } from 'reactstrap';
 
-function App() {
+const App = () => {
+
+  const changeModalState = (state) => {
+    setOpen(state)
+  }
+
+  const [isOpen, setOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App-header">
+      <div className="container">
+      <table className="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>
+        <Button
+        onClick={()=>setOpen(true)}
         >
-          Learn React
-        </a>
-      </header>
+          Mark
+        </Button>
+      </td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>
+      <Button
+      onClick={()=>setOpen(true)}
+      >
+          Otto
+      </Button>
+      </td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>
+      <Button
+      onClick={()=>setOpen(true)}
+      >
+          Larry
+      </Button>
+      </td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+      </div>
+      {(isOpen) && 
+    <ModalExample open={isOpen} state={changeModalState}/>
+    }
     </div>
   );
 }
